2f5de895020eec05180aac979742c679442ac879
tonga2402 <gbalmaceda@live.com>
2023-10-21 16:18:36 -0300

    style: :art: Responsive iphone 390x844

--- src/components/BodyRecomendado.jsx
+++ src/components/BodyRecomendado.jsx
@@ -14,8 +14,8 @@ function BodyRecomendado() {
 
 
       <div className={styles.recomendadoDetail}>
-        <h2>Lorem Ipsum</h2>
-        <p>
+        <h2 className={styles.recomendadoH2}>Lorem Ipsum</h2>
+        <p className={styles.recomendadoP}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam
           vestibulum morbi blandit cursus. Enim sed faucibus turpis in eu mi
