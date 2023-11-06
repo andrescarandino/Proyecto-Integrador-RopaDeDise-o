cfa4295e0c5415cb5aa7b67697928502e178ed24
Fernando Díaz <feeldiac@gmail.com>
2023-11-06 12:01:50 -0500

    feat: finish admin module

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/no-cycle
 export * from './useToast';
 export { default as useCreateProduct } from './useCreateProduct';
+export { default as useWindowDimensions } from './useWindowDimensions';
--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -1,5 +1,73 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import {
+	IconArrowNarrowRight,
+	IconBuildingStore,
+	IconUserFilled,
+} from '@tabler/icons-react';
+import { useNavigate } from 'react-router-dom';
+import * as createProductStyles from '../../styles/admin/createProduct.module.css';
+import * as menuStyles from '../../styles/admin/menu.module.css';
+import { useWindowDimensions } from '../../hooks';
+
 function AdminHome() {
-	return <h1>AdminHome</h1>;
+	const navigate = useNavigate();
+	const { width } = useWindowDimensions();
+
+	const goToCreateProduct = () => {
+		navigate('/admin/products/create');
+	};
+
+	return width > 768 ? (
+		<div className={createProductStyles.container}>
+			<header className={createProductStyles.headerContainer}>
+				<h2 className={createProductStyles.title}>
+					Panel de administración
+				</h2>
+			</header>
+			<section className={createProductStyles.sectionContainer}>
+				<p className={createProductStyles.sectionDescription}>
+					Estimado administrador. A continuación encontrarás el
+					listado de todas las funcionalidades de este panel
+					administrativo.
+				</p>
+
+				<nav className={menuStyles.navigation}>
+					<ul className={menuStyles.itemsContainer}>
+						<li
+							className={`${menuStyles.item} ${menuStyles.titleItem}`}
+						>
+							<IconUserFilled /> Usuarios
+						</li>
+						<li className={menuStyles.item}>
+							Listado de usuarios <IconArrowNarrowRight />
+						</li>
+						<li
+							className={`${menuStyles.item} ${menuStyles.titleItem}`}
+						>
+							<IconBuildingStore /> Productos
+						</li>
+						<li
+							className={menuStyles.item}
+							onClick={goToCreateProduct}
+							role="menuitem"
+						>
+							Agregar producto <IconArrowNarrowRight />
+						</li>
+						<li className={menuStyles.item}>
+							Listado de productos <IconArrowNarrowRight />
+						</li>
+					</ul>
+				</nav>
+			</section>
+		</div>
+	) : (
+		<div>
+			<p style={{ textAlign: 'center', color: 'red' }}>
+				🎈 No es posible acceder al panel administrativo desde un
+				dispositivo móvil.
+			</p>
+		</div>
+	);
 }
 
 export default AdminHome;
