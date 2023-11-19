050a6059d53c36df9c02d0602a6c4b69ab47017b
Fernando Díaz <feeldiac@gmail.com>
2023-11-19 11:32:49 -0500

    feat: finish manage of features in CreateProduct

--- src/pages/admin/CreateProduct.jsx
+++ src/pages/admin/CreateProduct.jsx
@@ -1,12 +1,18 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { IconBuildingStore } from '@tabler/icons-react';
-import { useState } from 'react';
+import {
+	IconBuildingStore,
+	IconPencil,
+	IconTrashFilled,
+} from '@tabler/icons-react';
+import { useState, Fragment } from 'react';
 import { useForm } from 'react-hook-form';
-import { useCreateProduct, useToast, useGetCategories } from '../../hooks';
 import CustomModal from '../../components/modal/CustomModal';
+import { useCreateProduct, useGetCategories, useToast } from '../../hooks';
 
 function CreateProduct() {
 	const toast = useToast();
+	const [selectedFeature, setSelectedFeature] = useState({});
 	const [openModal, setOpenModal] = useState(false);
 	const [preview, setPreview] = useState(null);
 	const {
@@ -17,18 +23,6 @@ function CreateProduct() {
 	} = useCreateProduct();
 	const { categories } = useGetCategories();
 
-	const onSubmit = (data) => {
-		createProductOnSubmit(data);
-
-		if (statusCode !== 200) {
-			toast.error('Error al crear el producto.');
-		}
-
-		// console.log({
-		// 	data,
-		// });
-	};
-
 	const handlePreview = ({ target: { files } }) => {
 		setPreview({
 			images: Array.from(files).map((file) => URL.createObjectURL(file)),
@@ -37,209 +31,358 @@ function CreateProduct() {
 
 	const {
 		register,
+		setValue,
+		watch,
 		handleSubmit,
 		formState: { errors },
-	} = useForm();
+	} = useForm({
+		defaultValues: {
+			features: [],
+		},
+	});
+
+	const watchFeatures = watch('features');
+
+	const onSubmit = (data) => {
+		console.log({
+			data,
+			errors,
+		});
+
+		createProductOnSubmit(data);
+
+		if (statusCode !== 200) {
+			toast.error('Error al crear el producto.');
+		}
+	};
+
+	const onErrors = (errors) => {
+		console.log({
+			errors,
+		});
+	};
+
+	const onSelectFeature = (selectedFeature) => {
+		if (!selectedFeature) return;
+		setSelectedFeature({
+			currentValue: selectedFeature,
+		});
+		setOpenModal(true);
+	};
+
+	const onAddFeature = () => {
+		setValue('features', [...watchFeatures, '']);
+	};
+
+	const onUpdateFeature = () => {
+		if (!selectedFeature?.newValue) return;
+
+		const updatedWatchFeatures = watchFeatures.map((feature) =>
+			feature === selectedFeature.currentValue
+				? selectedFeature.newValue
+				: feature,
+		);
+		setValue('features', updatedWatchFeatures);
+		setSelectedFeature({});
+		setOpenModal(false);
+	};
+
+	const onRemoveFeature = (selectedFeature) => {
+		const filteredWatchFeatures = watchFeatures.filter(
+			(feature) => feature !== selectedFeature,
+		);
+		setValue('features', filteredWatchFeatures);
+	};
 
 	return (
-		<div className="form-page-container">
-			<section className="section-container">
-				<header className="form-header">
-					<div className="form-header-icon-container">
-						<IconBuildingStore />
-					</div>
-					<div className="form-header-texts-container">
-						<h2 className="form-page-title">Agregar producto</h2>
-						<p className="section-description">
-							Estimado administrador, desde ésta sección podrás
-							crear nuevos productos para la plataforma.
-						</p>
-					</div>
-				</header>
-				<main className="form-container">
-					<form onSubmit={handleSubmit(onSubmit)} className="form">
-						<div className="form-group">
-							<label htmlFor="name">Escribe un nombre:</label>
-							<input
-								className="input"
-								id="name"
-								type="text"
-								autoComplete="off"
-								placeholder="Nombre"
-								{...register('name', { required: true })}
-							/>
+		<>
+			<div className="form-page-container">
+				<section className="section-container">
+					<header className="form-header">
+						<div className="form-header-icon-container">
+							<IconBuildingStore />
 						</div>
-						{errors.name && (
-							<span className="form-error-message">
-								Este campo es requerido.
-							</span>
-						)}
-						<div className="form-group">
-							<label htmlFor="description">
-								Escribe una descripción:
-							</label>
-							<input
-								className="input"
-								id="description"
-								type="text"
-								autoComplete="off"
-								placeholder="Descripción"
-								{...register('description', { required: true })}
-							/>
+						<div className="form-header-texts-container">
+							<h2 className="form-page-title">
+								Agregar producto
+							</h2>
+							<p className="section-description">
+								Estimado administrador, desde ésta sección
+								podrás crear nuevos productos para la
+								plataforma.
+							</p>
 						</div>
-						{errors.description && (
-							<span className="form-error-message">
-								Este campo es requerido.
-							</span>
-						)}
-						<div className="form-group">
-							<label htmlFor="category">
-								Selecciona una categoría:
-							</label>
-							<select
-								className="input"
-								id="category"
-								name="category"
-								{...register('category', { required: true })}
-							>
-								<option
-									// TODO: Style blank option
-									style={{
-										color: 'var(--color-oslo-grey) !important',
-										paddingInline: '0.5em !important',
-									}}
-									hidden
-									disabled
-									selected
+					</header>
+					<main className="form-container">
+						<form
+							onSubmit={handleSubmit(onSubmit, onErrors)}
+							className="form"
+						>
+							<div className="form-group">
+								<label htmlFor="name">Escribe un nombre:</label>
+								<input
+									className="input"
+									id="name"
+									type="text"
+									autoComplete="off"
+									placeholder="Nombre"
+									{...register('name', { required: true })}
+								/>
+							</div>
+							{errors.name && (
+								<span className="form-error-message">
+									Este campo es requerido.
+								</span>
+							)}
+							<div className="form-group">
+								<label htmlFor="description">
+									Escribe una descripción:
+								</label>
+								<textarea
+									className="input"
+									id="description"
+									type="text"
+									rows="5"
+									autoComplete="off"
+									placeholder="Descripción"
+									{...register('description', {
+										required: true,
+									})}
+								/>
+							</div>
+							{errors.description && (
+								<span className="form-error-message">
+									Este campo es requerido.
+								</span>
+							)}
+							<div className="form-group">
+								<label htmlFor="category">
+									Selecciona una categoría:
+								</label>
+								<select
+									className="input"
+									id="category"
+									name="category"
+									defaultValue=""
+									{...register('category', {
+										required: true,
+									})}
 								>
-									Selecciona una categoría
-								</option>
-								{categories?.map((category) => (
 									<option
-										key={category.id}
-										value={category.id}
-									>
-										{category.name}
-									</option>
-								))}
-							</select>
-						</div>
-						{errors.description && (
-							<span className="form-error-message">
-								Este campo es requerido.
-							</span>
-						)}
-						<div>
-							<CustomModal
-								openModal={openModal}
-								setOpenModal={setOpenModal}
-								contentComponent={
-									<div
-										// TODO: load default value of selected feature
+										// TODO: Style blank option
 										style={{
-											fontFamily: 'var(--ff-roboto)',
+											color: 'var(--color-oslo-grey) !important',
+											paddingInline: '0.5em !important',
 										}}
+										hidden
+										disabled
+										value=""
 									>
-										<header
-											style={{
-												color: 'var(--color-black-cow)',
-											}}
+										Selecciona una categoría
+									</option>
+									{categories?.map((category) => (
+										<option
+											key={category.id}
+											value={category.id}
 										>
-											<p
-												style={{
-													fontSize: '15px',
-													fontWeight: '600',
-													marginBlockEnd: '0.75rem',
-												}}
-											>
-												Editar característica
-												&quot;Ejemplo&quot;
-											</p>
-											<p
-												style={{
-													fontSize: '14px',
-												}}
-											>
-												Escribe a continuación el nuevo
-												valor des esta característica.
-											</p>
-										</header>
+											{category.name}
+										</option>
+									))}
+								</select>
+							</div>
+							{errors.description && (
+								<span className="form-error-message">
+									Este campo es requerido.
+								</span>
+							)}
+							<p
+								style={{
+									color: 'var(--color-black-cow)',
+									fontWeight: '600',
+									fontSize: '14px',
+									marginBlockStart: '1.75rem',
+									marginBlockEnd: '.25rem',
+									textAlign: 'end',
+								}}
+							>
+								Administrar características
+							</p>
+							<button
+								type="button"
+								className="submit-button"
+								onClick={onAddFeature}
+							>
+								Añadir nueva
+							</button>
+							{watchFeatures?.length > 0 &&
+								watchFeatures?.map((feature, index) => (
+									<Fragment key={index}>
 										<div className="form-group">
-											<label htmlFor="feature">
-												Característica:
+											<label
+												style={{
+													display: 'flex',
+													alignItems: 'center',
+												}}
+												htmlFor={`feature-${index}`}
+											>
+												<IconPencil
+													onClick={() =>
+														onSelectFeature(feature)
+													}
+												/>
+												<IconTrashFilled
+													onClick={() =>
+														onRemoveFeature(feature)
+													}
+												/>
+												&nbsp; Característica{' '}
+												{index + 1}:
 											</label>
 											<input
 												className="input"
-												id="feature"
+												id={`feature-${index}`}
 												type="text"
 												autoComplete="off"
-												placeholder="Característica"
-												{...register('feature', {
-													required: true,
-												})}
+												placeholder={`Característica ${
+													index + 1
+												}`}
+												{...register(
+													`features.${index}`,
+													{
+														required: true,
+													},
+												)}
 											/>
 										</div>
-										<div
-											style={{
-												display: 'flex',
-											}}
-										>
-											<button
-												type="button"
-												className="submit-button"
-											>
-												Guardar cambio
-											</button>
-										</div>
+										{errors.features?.at(index) && (
+											<span className="form-error-message">
+												Este campo es requerido.
+											</span>
+										)}
+									</Fragment>
+								))}
+							<div className="form-group">
+								<label htmlFor="image">
+									Selecciona las imágenes:
+								</label>
+								<input
+									id="image"
+									className="input"
+									type="file"
+									multiple
+									accept="image/*"
+									{...register(
+										'images',
+										{
+											onChange: handlePreview,
+										},
+										{ required: true },
+									)}
+								/>
+							</div>
+							{errors.images && (
+								<span className="form-error-message">
+									Este campo es requerido.
+								</span>
+							)}
+							{preview?.images?.length > 0 &&
+								preview?.images?.map((image, index) => (
+									<div
+										className="form-preview-item"
+										key={image}
+									>
+										<img
+											src={image}
+											alt={`Previsualización de la imagen ${
+												index + 1
+											}`}
+											key={image}
+											width={300}
+											height={300}
+										/>
 									</div>
-								}
-							/>
-						</div>
+								))}
+							<button type="submit" className="submit-button">
+								Agregar producto
+							</button>
+						</form>
+					</main>
+				</section>
+			</div>
+			<CustomModal
+				openModal={openModal}
+				setOpenModal={setOpenModal}
+				contentComponent={
+					<div
+						// TODO: load default value of selected feature
+						style={{
+							fontFamily: 'var(--ff-roboto)',
+						}}
+					>
+						<header
+							style={{
+								color: 'var(--color-black-cow)',
+							}}
+						>
+							<p
+								style={{
+									fontSize: '15px',
+									fontWeight: '600',
+									marginBlockEnd: '0.75rem',
+								}}
+							>
+								Editar característica &quot;
+								{selectedFeature?.currentValue}&quot;
+							</p>
+							<p
+								style={{
+									fontSize: '14px',
+								}}
+							>
+								Escribe a continuación el nuevo valor des esta
+								característica.
+							</p>
+						</header>
 						<div className="form-group">
-							<label htmlFor="image">
-								Selecciona las imágenes:
+							<label htmlFor="edit-feature">
+								Característica:
 							</label>
 							<input
-								id="image"
 								className="input"
-								type="file"
-								multiple
-								accept="image/*"
-								{...register(
-									'images',
-									{
-										onChange: handlePreview,
-									},
-									{ required: true },
-								)}
+								id="edit-feature"
+								type="text"
+								autoComplete="off"
+								placeholder="Característica"
+								onChange={(e) => {
+									setSelectedFeature({
+										...selectedFeature,
+										newValue: e.target.value,
+									});
+								}}
 							/>
 						</div>
-						{errors.images && (
+						{!selectedFeature?.newValue && (
 							<span className="form-error-message">
-								Este campo es requerido.
+								Escribe el nuevo valor de la característica.
 							</span>
 						)}
-						{preview?.images?.length > 0 &&
-							preview?.images?.map((image, index) => (
-								<div className="form-preview-item">
-									<img
-										src={image}
-										alt={`Previsualización de la imagen ${
-											index + 1
-										}`}
-										key={image}
-										width={300}
-										height={300}
-									/>
-								</div>
-							))}
-						<button type="submit" className="submit-button">
-							Agregar producto
-						</button>
-					</form>
-				</main>
-			</section>
-		</div>
+						<div
+							style={{
+								display: 'flex',
+							}}
+						>
+							<button
+								type="button"
+								className="submit-button"
+								onClick={onUpdateFeature}
+							>
+								Guardar cambio
+							</button>
+						</div>
+					</div>
+				}
+			/>
+		</>
 	);
 }
 
