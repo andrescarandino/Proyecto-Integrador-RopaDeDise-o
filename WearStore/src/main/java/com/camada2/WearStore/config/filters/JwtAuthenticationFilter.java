c23550a44fcc51dc6c93f2f9f812438b14f54a39
lixxx01 <oktubre0282@gmail.com>
2023-11-20 21:05:33 -0300

    Realizacion de Seguridad de usuarios

---
+++ WearStore/src/main/java/com/camada2/WearStore/config/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.camada2.WearStore.config.filters;
+
+import com.camada2.WearStore.config.jwt.JwtUtils;
+import com.camada2.WearStore.entity.Usuarios;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+
+    private JwtUtils jwtUtils;
+
+
+    public JwtAuthenticationFilter(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        Usuarios usuarios = null;
+
+        String email = "";
+        String password = "";
+
+        try {
+
+            usuarios = new ObjectMapper().readValue(request.getInputStream(), Usuarios.class);
+            email = usuarios.getEmail();
+            password = usuarios.getPassword();
+
+
+        } catch (StreamReadException e) {
+            throw new RuntimeException(e);
+        } catch (DatabindException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+        return getAuthenticationManager().authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        User user = (User) authResult.getPrincipal();
+        String token = jwtUtils.accessTokenGenerator(user.getUsername());
+
+        response.addHeader("Authorization", token);
+
+        Map<String, Object> httpResponse = new HashMap<>();
+        httpResponse.put("token", token);
+        httpResponse.put("Message", "Auttenticacion Correcta");
+        httpResponse.put("email", user.getUsername());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(httpResponse));
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().flush();//esto nos asegura que tod o se escriba correctamente
+
+
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
