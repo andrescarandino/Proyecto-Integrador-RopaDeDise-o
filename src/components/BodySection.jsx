82f7236b81d32843585c4d091857b0ac88cb2e26
tonga2402 <gbalmaceda@live.com>
2023-11-30 13:43:39 -0300

    refactor: :art: Categoria Home

--- src/components/BodySection.jsx
+++ src/components/BodySection.jsx
@@ -1,78 +1,60 @@
 import styles from '../styles/bodySection.module.css';
-import elegante from '../img/elegante.png';
-import casual from '../img/casual.png';
-import creativo from '../img/creativo.png';
-import dramatico from '../img/dramatico.png';
-import sexy from '../img/sexy.png';
-import romantico from '../img/romantico.png';
-import tradicional from '../img/tradicional.png';
-import vintage from '../img/vintage.png';
+import Abrigo from '../img/categoria_abrigo.png';
+import Blusa from '../img/categoria_blusa.png';
+import Camisa from '../img/categoria_camisa.png';
+import Camiseta from '../img/categoria_camiseta.png';
+import Pantalon from '../img/categoria_pantalon.png';
+import Jeans from '../img/categoria_jeans.png';
+import Saco from '../img/categoria_saco.png';
+import Vestido from '../img/categoria_vestido.png';
 
+const categoria = [
+	{
+		img: Abrigo,
+		nombre: 'Abrigo',
+	},
+	{
+		img: Blusa,
+		nombre: 'Blusa',
+	},
+	{
+		img: Camisa,
+		nombre: 'Camisa',
+	},
+	{
+		img: Camiseta,
+		nombre: 'Camiseta',
+	},
+	{
+		img: Pantalon,
+		nombre: 'Pantalon',
+	},
+	{
+		img: Jeans,
+		nombre: 'Jeans',
+	},
+	{
+		img: Saco,
+		nombre: 'Saco',
+	},
+	{
+		img: Vestido,
+		nombre: 'Vestido',
+	},
+];
 function BodySection() {
 	return (
 		<div className={styles.sectionContainer}>
-			<div className={styles.sectionFila}>
-				<div className={styles.sectionCard}>
-					<img src={elegante} alt="Categoría de ropa elegante" />
-					<button type="button" className={styles.sectionButton}>
-						Elegante
-					</button>
+			{categoria.map((x, index) => (
+				<div key={index} className={styles.sectionFila}>
+					<div className={styles.sectionCard}>
+						<img src={x.img} alt="Categoría de ropa elegante" />
+						<button type="button" className={styles.sectionButton}>
+							{x.nombre}
+						</button>
+					</div>
 				</div>
-				<div className={styles.sectionCard}>
-					<img src={sexy} alt="Categoría de ropa sexy" />
-					<button type="button" className={styles.sectionButton}>
-						Sexy
-					</button>
-				</div>
-			</div>
-
-			<div className={styles.sectionFila}>
-				<div className={styles.sectionCard}>
-					<img src={casual} alt="Categoría de ropa casual" />
-					<button type="button" className={styles.sectionButton}>
-						Casual
-					</button>
-				</div>
-				<div className={styles.sectionCard}>
-					<img src={romantico} alt="Categoría de ropa 'romántico'" />
-					<button type="button" className={styles.sectionButton}>
-						Romántico
-					</button>
-				</div>
-			</div>
-
-			<div className={styles.sectionFila}>
-				<div className={styles.sectionCard}>
-					<img src={creativo} alt="Categoría de ropa creativo" />
-					<button type="button" className={styles.sectionButton}>
-						Creativo
-					</button>
-				</div>
-				<div className={styles.sectionCard}>
-					<img
-						src={tradicional}
-						alt="Categoría de ropa tradicional"
-					/>
-					<button type="button" className={styles.sectionButton}>
-						Tradicional
-					</button>
-				</div>
-			</div>
-
-			<div className={styles.sectionFila}>
-				<div className={styles.sectionCard}>
-					<img src={dramatico} alt="Categoría de ropa dramático" />
-					<button type="button" className={styles.sectionButton}>
-						Dramático
-					</button>
-				</div>
-				<div className={styles.sectionCard}>
-					<img src={vintage} alt="Categoría de ropa vintage" />
-					<button type="button" className={styles.sectionButton}>
-						Vintage
-					</button>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 }
